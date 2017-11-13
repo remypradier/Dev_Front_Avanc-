359991304def7d4543535773f14c1e859e22f655
Rémy Pradier <remy.pradier@hetic.net>
2017-11-13 18:34:53 +0100

    Pendu v2

--- script/canvas.js
+++ script/canvas.js
@@ -1,3 +1,5 @@
+
+
   var Context = Canvas.getContext('2d');
   BuildCanvas();
 
@@ -44,75 +46,75 @@
   function MoveCanvas(Errors) {
     switch (Errors) {
       case 1:
-        //Head
-        Context.lineWidth = 5;
-        Context.beginPath();
-        Context.arc(240, 85, 30, 0, 2*Math.PI);
-        Context.stroke();
-        break;
+      //Head
+      Context.lineWidth = 5;
+      Context.beginPath();
+      Context.arc(240, 85, 30, 0, 2*Math.PI);
+      Context.stroke();
+      break;
 
       case 2:
-        //body
-        Context.moveTo(239, 115);
-        Context.lineTo(239, 210);
-        Context.stroke();
-        break;
+      //body
+      Context.moveTo(239, 115);
+      Context.lineTo(239, 210);
+      Context.stroke();
+      break;
 
       case 3:
-        //Left-arm
-        Context.moveTo(239, 140);
-        Context.lineTo(200, 180);
-        Context.stroke();
-        break;
+      //Left-arm
+      Context.moveTo(239, 140);
+      Context.lineTo(200, 180);
+      Context.stroke();
+      break;
 
       case 4:
-        //Right-arm
-        Context.moveTo(239, 140);
-        Context.lineTo(278, 180);
-        Context.stroke();
-        break;
+      //Right-arm
+      Context.moveTo(239, 140);
+      Context.lineTo(278, 180);
+      Context.stroke();
+      break;
 
       case 5:
-        //Left-Leg
-        Context.moveTo(239, 210);
-        Context.lineTo(200, 250);
-        Context.stroke();
-        break;
+      //Left-Leg
+      Context.moveTo(239, 210);
+      Context.lineTo(200, 250);
+      Context.stroke();
+      break;
 
       case 6:
-        //Right-Leg
-        Context.moveTo(239, 210);
-        Context.lineTo(278, 250);
-        Context.stroke();
-        break;
+      //Right-Leg
+      Context.moveTo(239, 210);
+      Context.lineTo(278, 250);
+      Context.stroke();
+      break;
 
       case 7:
-        //Face
-        Context.lineWidth = 3;
+      //Face
+      Context.lineWidth = 3;
 
-        //Left-eye
-        Context.moveTo(225, 73);
-        Context.lineTo(235, 83);
-        Context.stroke();
+      //Left-eye
+      Context.moveTo(225, 73);
+      Context.lineTo(235, 83);
+      Context.stroke();
 
-        Context.moveTo(225, 83);
-        Context.lineTo(235, 73);
-        Context.stroke();
+      Context.moveTo(225, 83);
+      Context.lineTo(235, 73);
+      Context.stroke();
 
-        //Right-eye
-        Context.moveTo(245, 73);
-        Context.lineTo(255, 83);
-        Context.stroke();
+      //Right-eye
+      Context.moveTo(245, 73);
+      Context.lineTo(255, 83);
+      Context.stroke();
 
-        Context.moveTo(245, 83);
-        Context.lineTo(255, 73);
-        Context.stroke();
+      Context.moveTo(245, 83);
+      Context.lineTo(255, 73);
+      Context.stroke();
 
-        //Mouse
-        Context.moveTo(232, 100);
-        Context.lineTo(250, 100);
-        Context.stroke();
-        break;
+      //Mouse
+      Context.moveTo(232, 100);
+      Context.lineTo(250, 100);
+      Context.stroke();
+      break;
       default:
     }
   }
--- script/script.js
+++ script/script.js
@@ -1,141 +1,196 @@
 var Canvas          = document.querySelector('canvas');
-var Replay          = document.querySelector('.replay');
-var Word            = document.querySelector('.word');
-var Title           = document.querySelector('.title');
-var keys            = document.querySelectorAll('.key');
-var LettersSelected = [];
-var WordToFind      = [];
-var WordBuild       = [];
-var WordsToFind     = [
-  'table',
-  'chien',
-  'porte',
-  'anticonstitutionnelement'
-];
-var RandomNumber;
-//var Letter;
-var Errors          = 0;
-var MaxErrors       = 7;
-var Continue        = true;
 
+(function (Canvas) {
+  var Replay          = document.querySelector('.replay');
+  var Word            = document.querySelector('.word');
+  var Title           = document.querySelector('.title');
+  var keys            = document.querySelectorAll('.key');
+  var LettersSelected = [];
+  var WordToFind      = [];
+  var WordBuild       = [];
+  var WordsToFind     = words;
+  var RandomNumber    = 0;
+  var Errors          = 0;
+  var MaxErrors       = 7;
+  var Continue        = true;
 
-WordToFind = Randomise(RandomNumber, WordToFind, WordBuild, Word);
+  Keyboard();
+  WordToFind = Randomise(RandomNumber, WordToFind, WordBuild, Word);
 
-document.addEventListener("keydown", function(event){
-  if(Continue) {
-    document.querySelector('.key_' + event.keyCode).classList.add('selected');
-    let Letter = String.fromCharCode(event.keyCode).toLowerCase();
-    test(Letter);
-  }
-})
+  document.addEventListener("keydown", function(event){
 
-for(var i = 0; i < keys.length; i++) {
-  keys[i].addEventListener('click', function() {
-    if(Continue) {
-      this.classList.add('selected');
-      let Letter = this.innerHTML;
-      test(Letter);
+    if(Continue && event.keyCode > 64 &&  event.keyCode < 91) {
+      document.querySelector('.key_' + event.keyCode).classList.add('selected');
+      let Letter = String.fromCharCode(event.keyCode).toLowerCase();
+      Test(Letter);
+    } else if(event.keyCode === 13) {
+      Init();
     }
   })
-}
 
-Replay.addEventListener('click', function() {
-  Init();
-  BuildCanvas();
-  WordToFind = Randomise(RandomNumber, WordToFind, WordBuild, Word)
-})
-
-function Init() {
-  LettersSelected = [];
-  WordToFind      = [];
-  WordBuild       = [];
-  Continue        = true;
-  Errors          = 0;
-  Title.innerHTML = "Jeu du pendu";
-  ResetKeyBoard();
-  DeleteCanvas();
-}
-
-function ResetKeyBoard() {
   for(var i = 0; i < keys.length; i++) {
-    if(keys[i].classList[2] === 'selected') {
-      keys[i].classList.remove('selected');
-    }
+    keys[i].addEventListener('click', function() {
+      if(Continue) {
+        this.classList.add('selected');
+        let Letter = this.innerHTML;
+        Test(Letter);
+      }
+    })
   }
-}
 
-function Randomise(RandomNumber, WordToFind, WordBuild, Word) {
-  RandomNumber = Math.floor(Math.random() * WordsToFind.length);
-  WordToFind = WordsToFind[RandomNumber].split('');
-  MakeWordBuild(Word, WordToFind, WordBuild);
-  return WordToFind;
+  Replay.addEventListener('click', function() {
+    Init();
+  })
 
-}
+  function Init() {
+    LettersSelected = [];
+    WordToFind      = [];
+    WordBuild       = [];
+    Continue        = true;
+    Errors          = 0;
+    Title.innerHTML = "Jeu du pendu";
+    Title.classList = "title";
+    Keyboard();
+    DeleteCanvas();
+    BuildCanvas();
+    WordToFind = Randomise(RandomNumber, WordToFind, WordBuild, Word)
+  }
 
-function test(Letter) {
-    if(CheckLetters(Letter,LettersSelected) === false) {
-      Errors = CheckLetterWord(Letter, WordToFind, WordBuild, LettersSelected, Errors);
-      if(Errors >= MaxErrors) {
-        Title.innerHTML = 'Perdu';
+  function Randomise(RandomNumber, WordToFind, WordBuild, Word) {
+    RandomNumber = Math.floor(Math.random() * WordsToFind.length);
+    WordToFind = WordsToFind[RandomNumber].split('');
+    MakeWordBuild(Word, WordToFind, WordBuild);
+    return WordToFind;
 
-        for(var i = 0; i < WordBuild.length; i++){
-          if(WordBuild[i] === '_') {
-            WordBuild[i] = "<span class='redletter'>" + WordToFind[i] + "</span>";
-            ShowWord(Word, WordBuild);
+  }
+
+  function Test(Letter) {
+      if(CheckLetters(Letter,LettersSelected) === false) {
+        Errors = CheckLetterWord(Letter, WordToFind, WordBuild, LettersSelected, Errors);
+        if(Errors >= MaxErrors) {
+          Title.innerHTML = 'Perdu';
+          Title.classList.add('redletter');
+
+          for(var i = 0; i < WordBuild.length; i++){
+            if(WordBuild[i] === '_') {
+              WordBuild[i] = "<span class='redletter'>" + WordToFind[i] + "</span>";
+              ShowWord(Word, WordBuild);
+            }
           }
-        }
 
-        Continue = false;
-        ShowReplay(Replay);
-      } else if (WordBuild.join(' ') == WordToFind.join(' ')) {
-        Title.innerHTML = "Bravo";
-        Continue = false;
-        ShowReplay(Replay);
+          Continue = false;
+          ShowReplay(Replay);
+        } else if (WordBuild.join(' ') == WordToFind.join(' ')) {
+          Title.innerHTML = "Bravo";
+          Title.classList.add('greenletter');
+          Continue = false;
+          ShowReplay(Replay);
+        }
+      }
+  }
+
+  function YouMad(Errors) {
+    Errors++;
+    MoveCanvas(Errors);
+    return Errors;
+  }
+
+  function CheckLetterWord(Letter, WordToFind, WordBuild, LettersSelected, Errors) {
+    let Save = WordBuild.slice(0);
+    for(var i = 0; i < WordToFind.length; i++) {
+      //http://aidenet.eu/grammaire01d.html
+      switch (Letter) {
+        case 'a':
+            switch (WordToFind[i]) {
+              case 'a': WordBuild[i] = 'a'; break;
+              case 'à': WordBuild[i] = 'à'; break;
+              case 'â': WordBuild[i] = 'â'; break;
+            }
+        break;
+        case 'e':
+            switch (WordToFind[i]) {
+              case 'e': WordBuild[i] = 'e'; break;
+              case 'é': WordBuild[i] = 'é'; break;
+              case 'è': WordBuild[i] = 'è'; break;
+              case 'ê': WordBuild[i] = 'ê'; break;
+              case 'ë': WordBuild[i] = 'ë'; break;
+            }
+        break;
+        case 'i':
+            switch (WordToFind[i]) {
+              case 'i': WordBuild[i] = 'i'; break;
+              case 'î': WordBuild[i] = 'î'; break;
+              case 'ï': WordBuild[i] = 'ï'; break;
+            }
+        break;
+        case 'o':
+            switch (WordToFind[i]) {
+              case 'o': WordBuild[i] = 'o'; break;
+              case 'ô': WordBuild[i] = 'ô'; break;
+            }
+        break;
+        case 'u':
+            switch (WordToFind[i]) {
+              case 'u': WordBuild[i] = 'u'; break;
+              case 'ù': WordBuild[i] = 'ù'; break;
+              case 'û': WordBuild[i] = 'û'; break;
+              case 'ü': WordBuild[i] = 'ü'; break;
+            }
+        break;
+        case 'c':
+            switch (WordToFind[i]) {
+              case 'c': WordBuild[i] = 'c'; break;
+              case 'ç': WordBuild[i] = 'ç'; break;
+            }
+        break;
+        case WordToFind[i]: WordBuild[i] = Letter; break;
       }
     }
-}
 
-function YouMad(Errors) {
-  Errors++;
-  MoveCanvas(Errors);
-  return Errors;
-}
 
-function CheckLetterWord(Letter, WordToFind, WordBuild, LettersSelected, Errors) {
-  let Save = WordBuild.slice(0);
-  for(var i = 0; i < WordToFind.length; i++) {
-    if(WordToFind[i] === Letter) {
-      WordBuild[i] = Letter;
+    LettersSelected.push(Letter);
+    if(Save.join(' ') === WordBuild.join(' ')) {
+      Errors = YouMad(Errors);
+    }
+    ShowWord(Word, WordBuild);
+    return Errors;
+  }
+
+  function CheckLetters(Letter, LettersSelected) {
+    if(LettersSelected.indexOf(Letter) !== -1) {
+      return(true);
+    } else {
+      return(false);
     }
   }
-  LettersSelected.push(Letter);
-  if(Save.join(' ') === WordBuild.join(' ')) {
-    Errors = YouMad(Errors);
+
+  function MakeWordBuild(Word, WordToFind, WordBuild) {
+    for(var i = 0; i < WordToFind.length; i++) {
+      if(WordToFind[i] === '-') {
+        WordBuild.push('-');
+      } else {
+        WordBuild.push('_');
+      }
+    };
+    ShowWord(Word, WordBuild);
   }
-  ShowWord(Word, WordBuild);
-  return Errors;
-}
 
-function CheckLetters(Letter, LettersSelected) {
-  if(LettersSelected.indexOf(Letter) !== -1) {
-    return(true);
-  } else {
-    return(false);
+  function Keyboard() {
+    let Keyboard = document.querySelector('.keyboard');
+    Keyboard.innerHTML = '';
+    for (var i = 65; i < 91; i++) {
+      Keyboard.innerHTML += '<li class="key key_' + i + '">' +
+        String.fromCharCode(i) + '</li>'
+    }
   }
-}
 
-function MakeWordBuild(Word, WordToFind, WordBuild) {
-  for(var i = 0; i < WordToFind.length; i++) {
-    WordBuild.push('_');
-  };
-  ShowWord(Word, WordBuild);
-}
+  function ShowWord(Word, WordBuild, ) {
+    Word.innerHTML = WordBuild.join(' ');
+  }
 
-function ShowWord(Word, WordBuild, ) {
-  Word.innerHTML = WordBuild.join(' ');
-}
+  function ShowReplay(Replay, RandomNumber, WordToFind, WordBuild, Word) {
+    Replay.style.visibility = 'visible';
 
-function ShowReplay(Replay, RandomNumber, WordToFind, WordBuild, Word) {
-  Replay.style.visibility = 'visible';
+  }
 
-}
+})();
